d1136e0e082185dd01f8ad676699e6e499cab481
Matthew Standish <mwstandish@aol.com>
2023-07-04 16:23:19 -0400

    updated models

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require("../../controllers/thoughtController");
+
+router.get("/", getAllThoughts);
+router.get("/:id", getThoughtById);
+router.post("/", createThought);
+router.put("/:id", updateThought);
+router.delete("/:id", deleteThought);
+router.post("/:thoughtId/reactions", createReaction);
+router.delete("/:thoughtId/reactions/:reactionId", deleteReaction);
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require("../../controllers/userController");
+
+router.get("/", getAllUsers);
+router.get("/:id", getUserById);
+router.post("/", createUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+router.post("/:userId/friends/:friendId", addFriend);
+router.delete("/:userId/friends/:friendId", removeFriend);
+
+module.exports = router;
